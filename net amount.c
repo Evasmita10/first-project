a32a66125ea26a3f141a59e3cfa6847abaf9dcd6
Evasmita10 <evaaasarma@gmail.com>
2024-10-26 20:50:18 +0530

    Add files via upload

---
+++ net amount.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int price;
+    float c;
+    printf("Enter price=100);
+    scanf("%d, &price);
+    discount=price*0.15
+    c=price-discount
+    printf("%f %f",discount,c);
+    return 0;
+    }
